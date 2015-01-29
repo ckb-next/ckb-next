e16f7618561ee61d5850364c33fef0d0ad57e181
ccMSC <qccmsc@gmail.com>
2015-01-29 00:08:49 -0700

    (ckb-gradient) Hold gradient color as long as a key is held down

--- src/ckb-gradient/main.c
+++ src/ckb-gradient/main.c
@@ -4,7 +4,7 @@
 void ckb_info(){
     // Plugin info
     CKB_NAME("Gradient");
-    CKB_VERSION("0.6");
+    CKB_VERSION("0.7");
     CKB_COPYRIGHT("2014", "MSC");
     CKB_LICENSE("GPLv2");
     CKB_GUID("{54DD2975-E192-457D-BCFC-D912A24E33B4}");
@@ -12,6 +12,7 @@ void ckb_info(){
 
     // Effect parameters
     CKB_PARAM_AGRADIENT("color", "Color:", "", "ffffffff");
+    CKB_PARAM_BOOL("kphold", "Freeze until key is released", TRUE);
 
     // Timing/input parameters
     CKB_KPMODE(CKB_KP_NAME);
@@ -22,8 +23,11 @@ void ckb_info(){
     CKB_DEFAULT_TRIGGER_KP(TRUE);
 }
 
+#define NONE -1.f
+#define HOLD -2.f
+
 ckb_gradient animcolor = { 0 };
-int kprelease = 0;
+int kphold = 0, kprelease = 0;
 float* target = 0;
 
 void ckb_init(ckb_runctx* context){
@@ -31,20 +35,28 @@ void ckb_init(ckb_runctx* context){
     unsigned count = context->keycount;
     target = malloc(count * sizeof(float));
     for(unsigned i = 0; i < count; i++)
-        target[i] = -1.f;
+        target[i] = NONE;
 }
 
 void ckb_parameter(ckb_runctx* context, const char* name, const char* value){
     CKB_PARSE_AGRADIENT("color", &animcolor){}
+    CKB_PARSE_BOOL("kphold", &kphold){}
     CKB_PARSE_BOOL("kprelease", &kprelease){}
 }
 
 void ckb_keypress(ckb_runctx* context, ckb_key* key, int x, int y, int state){
     // Start or stop animation on key
-    if(state)
-        target[key - context->keys] = 0.f;
-    else if(kprelease)
-        target[key - context->keys] = 1.f;
+    if(state){
+        if(kphold)
+            target[key - context->keys] = HOLD;
+        else
+            target[key - context->keys] = 0.f;
+    } else {
+        if(kprelease)
+            target[key - context->keys] = NONE;
+        else if(kphold)
+            target[key - context->keys] = 0.f;
+    }
 }
 
 void ckb_start(ckb_runctx* context){
@@ -57,11 +69,15 @@ int ckb_frame(ckb_runctx* context, double delta){
     unsigned count = context->keycount;
     for(unsigned i = 0; i < count; i++){
         float phase = target[i];
-        if(phase > 1.f || phase < 0.f){
-            phase = -1.f;
-            continue;
+        if(phase == HOLD)
+            phase = 0.f;
+        else {
+            if(phase > 1.f || phase < 0.f){
+                phase = -1.f;
+                continue;
+            }
+            target[i] = (phase += delta);
         }
-        target[i] = (phase += delta);
         ckb_key* key = context->keys + i;
         float a, r, g, b;
         ckb_grad_color(&a, &r, &g, &b, &animcolor, phase * 100.);

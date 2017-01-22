09c6b1385d803f1842d5d4781dac57fedde5b520
Oleksii Vilchanskyi <alexvilchansky@yahoo.com>
2017-01-23 00:51:05 +0100

    cmake: reflect current progress

---
+++ cmake/BuildLocation.cmake
@@ -0,0 +1,11 @@
+# Set up our directory structure for output libraries and binaries
+if(NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
+endif()
+if(NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
+    if(UNIX)
+        set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib")
+    else()
+        set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
+    endif()
+endif()

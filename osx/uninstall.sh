10bfe92d4435698ebd9ec091e6a74fd918b86a90
Oleksii Vilchanskyi <alexvilchansky@yahoo.com>
2017-02-16 18:32:16 +0100

    add options, reformat files, change install dirs

--- osx/uninstall.sh
+++ osx/uninstall.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+echo "Stopping ckb-next app"
+/Applications/ckb-next.app/Contents/MacOS/ckb-next --close >/dev/null 2>&1
+echo "Removing ckb-next app"
+sudo rm -rf /Applications/ckb-next.app >/dev/null 2>&1
 echo "Stopping daemon"
-sudo launchctl unload /Library/LaunchDaemons/ckb-next-daemon.plist
+sudo launchctl unload /Library/LaunchDaemons/ckb-next-daemon.plist >/dev/null 2>&1
 echo "Removing daemon's plist"
-sudo rm -rf /Library/LaunchDaemons/ckb-next-daemon.plist
-echo "Stopping ckb-next"
-/Applications/ckb-next.app/Contents/MacOS/ckb-next --close
-echo "Removing ckb-next"
-sudo rm -rf /Applications/ckb-next.app
+sudo rm -rf /Library/LaunchDaemons/ckb-next-daemon.plist >/dev/null 2>&1

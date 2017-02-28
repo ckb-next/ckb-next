04d11f27026a9414ad520e89af2fbc3885e04ae9
Oleksii Vilchanskyi <alexvilchansky@yahoo.com>
2017-02-28 21:59:39 +0100

    no sudo no cry

--- linux/uninstall.sh
+++ linux/uninstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Stopping ckb-next app"
-/usr/bin/ckb-next --close >/dev/null 2>&1
-/usr/bin/ckb --close >/dev/null 2>&1
+pkill ckb-next >/dev/null 2>&1
+pkill ckb >/dev/null 2>&1
 
 echo "Detecting system"
 systemd=$(systemctl --version 2>/dev/null)
@@ -13,30 +13,30 @@ openrcc=$?
 if [[ $systemdc -eq 0 && $(echo "$systemd" | wc -l) -ge 1 ]]; then
     echo "System service: systemd detected"
     echo "Stopping and removing service files"
-    sudo systemctl stop ckb-daemon 2>/dev/null
-    sudo systemctl disable ckb-daemon 2>/dev/null
-    sudo rm -f /usr/lib/systemd/system/ckb-daemon.service 2>/dev/null
-    sudo systemctl daemon-reload 2>/dev/null
-    sudo systemctl stop ckb-next-daemon 2>/dev/null
-    sudo systemctl disable ckb-next-daemon 2>/dev/null
-    sudo rm -f /usr/lib/systemd/system/ckb-next-daemon.service 2>/dev/null
-    sudo systemctl daemon-reload 2>/dev/null
+    systemctl stop ckb-daemon 2>/dev/null
+    systemctl disable ckb-daemon 2>/dev/null
+    rm -f /usr/lib/systemd/system/ckb-daemon.service 2>/dev/null
+    systemctl daemon-reload 2>/dev/null
+    systemctl stop ckb-next-daemon 2>/dev/null
+    systemctl disable ckb-next-daemon 2>/dev/null
+    rm -f /usr/lib/systemd/system/ckb-next-daemon.service 2>/dev/null
+    systemctl daemon-reload 2>/dev/null
 elif [[ $upstartc -eq 0 && $(echo "$upstart" | wc -l) -ge 1 ]]; then
     echo "System service: Upstart detected"
     echo "Stopping and removing service files"
-    sudo service ckb-daemon stop 2>/dev/null
-    sudo rm -f /etc/init/ckb-daemon.conf 2>/dev/null
-    sudo service ckb-next-daemon stop 2>/dev/null
-    sudo rm -f /etc/init/ckb-next-daemon.conf 2>/dev/null
+    service ckb-daemon stop 2>/dev/null
+    rm -f /etc/init/ckb-daemon.conf 2>/dev/null
+    service ckb-next-daemon stop 2>/dev/null
+    rm -f /etc/init/ckb-next-daemon.conf 2>/dev/null
 elif [[ $openrcc -eq 0 && $(echo "$openrc" | wc -l) -ge 1 ]]; then
     echo "System service: openrc detected"
     echo "Stopping and removing service files"
-    sudo rc-service ckb-daemon stop 2>/dev/null
-    sudo rc-update del ckb-daemon default 2>/dev/null
-    sudo rm -f /etc/init.d/ckb-daemon 2>/dev/null
-    sudo rc-service ckb-next-daemon stop 2>/dev/null
-    sudo rc-update del ckb-next-daemon default 2>/dev/null
-    sudo rm -f /etc/init.d/ckb-next-daemon 2>/dev/null
+    rc-service ckb-daemon stop 2>/dev/null
+    rc-update del ckb-daemon default 2>/dev/null
+    rm -f /etc/init.d/ckb-daemon 2>/dev/null
+    rc-service ckb-next-daemon stop 2>/dev/null
+    rc-update del ckb-next-daemon default 2>/dev/null
+    rm -f /etc/init.d/ckb-next-daemon 2>/dev/null
 else
     echo "No supported system service daemons detected."
 fi

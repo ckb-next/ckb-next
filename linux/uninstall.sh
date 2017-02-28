884b074076bbed158b761a4cd3eeb71a0e555276
Oleksii Vilchanskyi <alexvilchansky@yahoo.com>
2017-02-28 22:01:35 +0100

    add check uninstall runs from root

--- linux/uninstall.sh
+++ linux/uninstall.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 echo "Stopping ckb-next app"
 pkill ckb-next >/dev/null 2>&1
 pkill ckb >/dev/null 2>&1

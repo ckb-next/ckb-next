a2b65081d028b27c79f101fd71e7b5b25915bfbb
Lutz Eichler <github@frickler24.de>
2017-05-06 20:16:42 +0200

    Add reset of usb devices in case of misbehaving
    In several cases we have seen error messages in the logfiles like
    
    USBDEVFS_CONTROL failed cmd ckb-daemon rqt 33 rq 9 len 64 ret -110
    
    [W] _start_dev (device.c:24): Unable to load firmware version/poll rate
    
    [E] os_usbsend (via firmware.c:15): Connection timed out
    
    or similar.
    
    This happens if the device is in a status where the communication
    between linux and device is corrupted. E.g. with an K95RGB one may
    provoke the behavior by moving the poll-rate-switch, eg to BIOS mode.
    
    This fix adds some USB-resets for the device if
    either ioctls for getting infos give bad return values
    or the device does not provide valuable information about endpoints,
    version numbers etc.

--- src/ckb-daemon/usb_linux.c
+++ src/ckb-daemon/usb_linux.c
@@ -211,11 +211,16 @@ int _nk95cmd(usbdevice* kb, uchar bRequest, ushort wValue, const char* file, int
 ///
 /// \n The ioctl command is USBDEVFS_CONTROL.
 ///
-void os_sendindicators(usbdevice* kb){
+void os_sendindicators(usbdevice* kb) {
+    static int countForReset = 0;
     struct usbdevfs_ctrltransfer transfer = { 0x21, 0x09, 0x0200, 0x00, 1, 500, &kb->ileds };
     int res = ioctl(kb->handle - 1, USBDEVFS_CONTROL, &transfer);
-    if(res <= 0)
+    if(res <= 0) {
         ckb_err("%s\n", res ? strerror(errno) : "No data written");
+        if (usb_tryreset(kb) == 0 && countForReset++ < 3) {
+            os_sendindicators(kb);
+        }
+    }
 }
 
 ///
@@ -564,7 +569,13 @@ int os_setupusb(usbdevice* kb) {
         sscanf(ep_str, "%d", &kb->epcount);
     if(kb->epcount < 2){
         // IF we have an RGB KB with 0 or 1 endpoints, it will be in BIOS mode.
-        ckb_err("Possibly unable to read endpoint count from udev, assuming %d and reading >>%s<<...\n", kb->epcount, ep_str);
+        ckb_err("Unable to read endpoint count from udev, assuming %d and reading >>%s<< or device is in BIOS mode\n", kb->epcount, ep_str);
+        if (usb_tryreset(kb) == 0) { ///< Try to reset the device and recall the function
+            static int retryCount = 0; ///< Don't do this endless in recursion
+            if (retryCount++ < 5) {
+                return os_setupusb(kb); ///< os_setupusb() has a return value (used as boolean)
+            }
+        }
         return -1;
         // ToDo are there special versions we have to detect? If there are, that was the old code to handle it:
         // This shouldn't happen, but if it does, assume EP count based onckb_warn what the device is supposed to have

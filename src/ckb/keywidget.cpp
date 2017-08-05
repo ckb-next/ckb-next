62e60606c19c49e3a8e971cec949d1f6218be34a
Tasos Sahanidis <tasos@tasossah.com>
2017-08-05 22:03:54 +0300

    Initial import of Harpoon overlay

--- src/ckb/keywidget.cpp
+++ src/ckb/keywidget.cpp
@@ -9,7 +9,7 @@
 
 static const int KEY_SIZE = 12;
 
-static QImage* m65Overlay = 0, *sabOverlay = 0, *scimOverlay = 0;
+static QImage* m65Overlay = 0, *sabOverlay = 0, *scimOverlay = 0, *harpOverlay = 0;
 
 // KbLight.cpp
 extern QRgb monoRgb(float r, float g, float b);
@@ -131,6 +131,12 @@ void KeyWidget::paintEvent(QPaintEvent*){
             if(!scimOverlay)
                 scimOverlay = new QImage(":/img/overlay_scimitar.png");
             overlay = scimOverlay;
+            xpos = 3.5f;
+            ypos = -2.f;
+        } else if(model == KeyMap::HARPOON){
+            if(!harpOverlay)
+                harpOverlay = new QImage(":/img/overlay_harpoon.png");
+            overlay = harpOverlay;
             xpos = 3.5f;
             ypos = -2.f;
         }

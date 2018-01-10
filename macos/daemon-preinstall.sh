52e1e30a8efc3832f4d0a9a8a8ceea311a9123b5
Oleksii Vilchanskyi <oleksii.vilchanskyi@gmail.com>
2018-01-10 18:54:23 +0100

    Add macOS packaging

---
+++ macos/daemon-preinstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# old plist
+sudo launchctl unload -w /Library/LaunchDaemons/com.ckb.daemon.plist
+# new plist
+sudo launchctl unload -w /Library/LaunchDaemons/org.next.ckb.daemon.plist
+exit 0

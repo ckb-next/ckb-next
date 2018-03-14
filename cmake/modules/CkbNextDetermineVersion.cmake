490180e78e007fe6176d91807f0c0217f2d81eb0
Tasos Sahanidis <tasos@tasossah.com>
2018-03-15 00:09:43 +0200

    Add fallback for non-release zip downloads

--- cmake/modules/CkbNextDetermineVersion.cmake
+++ cmake/modules/CkbNextDetermineVersion.cmake
@@ -50,50 +50,58 @@ function(determine_version source_dir git_command var_prefix)
     set(full)
     set(patch_extra)
 
-    if (EXISTS ${git_command})
-        execute_process(
-                COMMAND ${git_command} describe
-                WORKING_DIRECTORY ${source_dir}
-                RESULT_VARIABLE result
-                OUTPUT_VARIABLE output
-                ERROR_QUIET
-                OUTPUT_STRIP_TRAILING_WHITESPACE)
-        if (${result} EQUAL 0)
-            string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)[-]*(.*)"
-                    version_matches ${output})
-            if (CMAKE_MATCH_0)
-                # message(STATUS "Determined Git Version: ${CMAKE_MATCH_0}")
-                set(full ${CMAKE_MATCH_0})
-                set(major ${CMAKE_MATCH_1})
-                set(minor ${CMAKE_MATCH_2})
-                set(patch ${CMAKE_MATCH_3})
-                set(patch_extra ${CMAKE_MATCH_4})
+    if (NOT ${${var_prefix}_VERSION_IS_RELEASE})
+        if (EXISTS ${git_command})
+            execute_process(
+                    COMMAND ${git_command} describe
+                    WORKING_DIRECTORY ${source_dir}
+                    RESULT_VARIABLE result
+                    OUTPUT_VARIABLE output
+                    ERROR_QUIET
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+            if (${result} EQUAL 0)
+                string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)[-]*(.*)"
+                        version_matches ${output})
+                if (CMAKE_MATCH_0)
+                    # message(STATUS "Determined Git Version: ${CMAKE_MATCH_0}")
+                    set(full ${CMAKE_MATCH_0})
+                    set(major ${CMAKE_MATCH_1})
+                    set(minor ${CMAKE_MATCH_2})
+                    set(patch ${CMAKE_MATCH_3})
+                    set(patch_extra ${CMAKE_MATCH_4})
+                endif ()
+            else ()
+                message("\
+    Git directory to deduce precise version has not been not found.\n\
+    Consider cloning the project with git instead of downloading an archive.\n\
+    For example:\n\
+        git clone -b master --single-branch https://github.com/ckb-next/ckb-next.git")
             endif ()
         else ()
-            message("\
-Git directory to deduce precise version has not been not found. \
-Consider cloning the project with git instead of downloading an archive. \
-For example: \
-    git clone -b master --single-branch https://github.com/mattanger/ckb-next.git")
+            message("Git executable not found.")
         endif ()
-    else ()
-        message("Git executable not found.")
-    endif ()
 
-    if (full)
-        set(${var_prefix}_VERSION ${full} PARENT_SCOPE)
-        set(${var_prefix}_VERSION_MAJOR ${major} PARENT_SCOPE)
-        set(${var_prefix}_VERSION_MINOR ${minor} PARENT_SCOPE)
-        set(${var_prefix}_VERSION_PATCH ${patch} PARENT_SCOPE)
-        set(${var_prefix}_VERSION_PATCH_EXTRA ${patch_extra} PARENT_SCOPE)
-    endif ()
+        if (full)
+            set(${var_prefix}_VERSION ${full} PARENT_SCOPE)
+            set(${var_prefix}_VERSION_MAJOR ${major} PARENT_SCOPE)
+            set(${var_prefix}_VERSION_MINOR ${minor} PARENT_SCOPE)
+            set(${var_prefix}_VERSION_PATCH ${patch} PARENT_SCOPE)
+            set(${var_prefix}_VERSION_PATCH_EXTRA ${patch_extra} PARENT_SCOPE)
 
-    if ("${major}.${minor}.${patch}" STREQUAL "${full}")
-        set(${var_prefix}_VERSION_IS_RELEASE TRUE PARENT_SCOPE)
-        message(STATUS "${var_prefix} version: ${full} (Release)")
-    else ()
-        set(${var_prefix}_VERSION_IS_RELEASE FALSE PARENT_SCOPE)
-        message(STATUS "${var_prefix} version: ${full} (Non-release)")
-    endif ()
+            if ("${major}.${minor}.${patch}" STREQUAL "${full}")
+                set(${var_prefix}_VERSION_IS_RELEASE TRUE PARENT_SCOPE)
+                message(STATUS "${var_prefix} version: ${full} (Release)")
+            else ()
+                set(${var_prefix}_VERSION_IS_RELEASE FALSE PARENT_SCOPE)
+                message(STATUS "${var_prefix} version: ${full} (Non-release)")
+            endif ()
+
+        else()
+            set(${var_prefix}_VERSION ${full} "${${var_prefix}_VERSION}-unknown" PARENT_SCOPE)
+            set(${var_prefix}_VERSION_IS_RELEASE FALSE PARENT_SCOPE)
+            message(STATUS "${var_prefix} version: ${${var_prefix}_VERSION}-unknown (Non-release)")
+        endif ()
+
+    endif()
 
 endfunction()

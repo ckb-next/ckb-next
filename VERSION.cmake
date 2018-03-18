796c7c4e81edcb8bb56567f04fe51ff95dc946ee
Tasos Sahanidis <tasos@tasossah.com>
2018-03-18 03:36:59 +0200

    Bump up dev version to 0.3.0

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,6 +1,6 @@
-set(ckb-next_VERSION "0.2.9")
+set(ckb-next_VERSION "0.3.0")
 set(ckb-next_VERSION_MAJOR 0)
-set(ckb-next_VERSION_MINOR 2)
-set(ckb-next_VERSION_PATCH 9)
+set(ckb-next_VERSION_MINOR 3)
+set(ckb-next_VERSION_PATCH 0)
 # This should be set to TRUE _only_ in archive/tarball releases
 set(ckb-next_VERSION_IS_RELEASE FALSE)

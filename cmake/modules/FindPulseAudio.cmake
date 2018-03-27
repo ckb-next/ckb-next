5f1ade2e7a9d39097bc7eb1792413b315474ba7a
Tasos Sahanidis <tasos@tasossah.com>
2018-03-27 18:40:45 +0300

    Fix libpulse linking

--- cmake/modules/FindPulseAudio.cmake
+++ cmake/modules/FindPulseAudio.cmake
@@ -35,8 +35,6 @@
 #       PULSEAUDIOSIMPLE_INCLUDE_DIRS - the pulseaudio simple include directory
 #       PULSEAUDIOSIMPLE_LIBRARIES - Link these to use pulseaudio simple
 
-# TODO: Fix this properly
-set(PULSEAUDIO_LIBRARY "")
 foreach (pulsevariablename PULSEAUDIOSIMPLE PULSEAUDIO)
     find_path(${pulsevariablename}_INCLUDE_DIR
             NAMES

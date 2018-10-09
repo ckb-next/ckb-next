487d2d9b188aff9ce31fe60af52b0c7535da7c57
Tasos Sahanidis <tasos@tasossah.com>
2018-10-09 15:56:04 +0300

    Set IS_RELEASE flag to false

--- VERSION.cmake
+++ VERSION.cmake
@@ -3,7 +3,7 @@ set(ckb-next_VERSION_MAJOR 0)
 set(ckb-next_VERSION_MINOR 3)
 set(ckb-next_VERSION_PATCH 2)
 # This should be set to TRUE _only_ in archive/tarball releases
-set(ckb-next_VERSION_IS_RELEASE TRUE)
+set(ckb-next_VERSION_IS_RELEASE FALSE)
 
 # Profile format version
 set(ckb-next_PROFILE_VER 1)

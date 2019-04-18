a2b5061be31295daacb0c4fcecab12c706975497
Tasos Sahanidis <tasos@tasossah.com>
2019-04-19 02:36:22 +0300

    Add mac uninstall script to the package

--- macos/uninstall.sh
+++ macos/uninstall.sh
@@ -7,8 +7,11 @@ open -a 'ckb-next' --args --close
 echo "Stopping the daemon and removing service file"
 sudo launchctl unload /Library/LaunchDaemons/ckb-next-daemon.plist
 sudo launchctl unload /Library/LaunchDaemons/com.ckb.daemon.plist
+sudo launchctl unload /Library/LaunchDaemons/org.ckb-next.daemon.plist
 sudo rm -f /Library/LaunchDaemons/ckb-next-daemon.plist
 sudo rm -f /Library/LaunchDaemons/com.ckb.daemon.plist
+sudo rm -f /Library/LaunchDaemons/org.ckb-next.daemon.plist
 
 echo "Removing GUI, daemon and support files"
 sudo rm -rf /Applications/ckb{,-next}.app
+sudo rm "/Library/Application Support/ckb-next-daemon"

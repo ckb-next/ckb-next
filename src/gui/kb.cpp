f59cb33817189dc13a1201b1cf518c6e8d59e316
Tasos Sahanidis <tasos@tasossah.com>
2019-08-21 17:00:36 +0300

    Filter more words in device name

--- src/gui/kb.cpp
+++ src/gui/kb.cpp
@@ -53,7 +53,7 @@ Kb::Kb(QObject *parent, const QString& path) :
         hwload = true;
     if (mpath.open(QIODevice::ReadOnly)){
         usbModel = mpath.read(100);
-        usbModel = usbModel.remove("Corsair").remove("Gaming").remove("Keyboard").remove("Mouse").remove("Bootloader").trimmed();
+        usbModel = usbModel.remove("Corsair", Qt::CaseInsensitive).remove("Gaming").remove("Keyboard").remove("Mouse").remove("Bootloader").remove("Mechanical").replace("LOW PROFILE", "LP").trimmed();
         mpath.close();
     }
     if (usbModel == "")

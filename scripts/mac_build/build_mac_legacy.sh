63f9773fd72c9e156facc2146179f1e00c0fc57e
Tasos Sahanidis <tasos@tasossah.com>
2019-10-08 14:22:26 +0300

    Add Qt PATH check in legacy script

--- scripts/mac_build/build_mac_legacy.sh
+++ scripts/mac_build/build_mac_legacy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [[ "$PATH" == *"/usr/local/opt/qt/bin"* ]]; then
+    echo "Brew's Qt is in your PATH (/usr/local/opt/qt/bin). Please remove it and try again."
+    exit
+fi
+
 mkdir build
 cd build
-Qt5_DIR=~/Qt5.6.2/5.6/clang_64/ cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 -DCMAKE_BUILD_TYPE=Release -DSAFE_INSTALL=ON -DSAFE_UNINSTALL=ON -DMAC_LEGACY=1 .. -DUSE_BREW_QUAZIP=0 -DUSE_BREW_QT5=0 -DUSE_PORTAUDIO=1 -DWITH_MVIZ=1
+Qt5_DIR=~/Qt5.6.3/5.6/clang_64/ cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 -DCMAKE_BUILD_TYPE=Release -DSAFE_INSTALL=ON -DSAFE_UNINSTALL=ON -DMAC_LEGACY=1 .. -DUSE_BREW_QUAZIP=0 -DUSE_BREW_QT5=0 -DUSE_PORTAUDIO=1 -DWITH_MVIZ=1
 make -j4 macos-package

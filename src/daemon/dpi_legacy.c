fb741d28271c707eda2daf9e2288c2530d95e626
Tasos Sahanidis <tasos@tasossah.com>
2020-03-14 20:58:11 +0200

    Add M45 command for daemon to set the DPI stage

--- src/daemon/dpi_legacy.c
+++ src/daemon/dpi_legacy.c
@@ -43,30 +43,40 @@ int updatedpi_legacy(usbdevice* kb, int force){
     dpiset* lastdpi = &kb->profile->lastdpi;
     dpiset* newdpi = &kb->profile->currentmode->dpi;
     // Don't do anything if the settings haven't changed
-    if(!force && !lastdpi->forceupdate && !newdpi->forceupdate
-            && !memcmp(lastdpi->x, newdpi->x, sizeof(ushort) * DPI_COUNT)
-            && !memcmp(lastdpi->y, newdpi->y, sizeof(ushort) * DPI_COUNT))
-        return 0;
-    lastdpi->forceupdate = newdpi->forceupdate = 0;
+    if(force || lastdpi->forceupdate || newdpi->forceupdate
+            || memcmp(lastdpi->x, newdpi->x, sizeof(ushort) * DPI_COUNT)
+            || memcmp(lastdpi->y, newdpi->y, sizeof(ushort) * DPI_COUNT))
+    {
+        lastdpi->forceupdate = newdpi->forceupdate = 0;
 
-    // Build DPI packet
-    // 0x04 0x02 Y1 X1 Y2 X2 Y3 X3 YS XS
-    uchar dpi_pkt[10] = { 0x04, 0x02, 0 };
+        // Build DPI packet
+        // 0x04 0x02 Y1 X1 Y2 X2 Y3 X3 YS XS
+        uchar dpi_pkt[10] = { 0x04, 0x02, 0 };
 
-    // M95 has 3 stages + sniper
-    int pos = 2;
-    for(int i = 1; i < 4; i++){
-        // Offset to skip the header, Y is first
-        // Round to the next DPI
-        dpi_pkt[pos++] = newdpi->y[i] / 50;
-        dpi_pkt[pos++] = newdpi->x[i] / 50;
+        // M95 has 3 stages + sniper
+        int pos = 2;
+        for(int i = 1; i < 4; i++){
+            // Offset to skip the header, Y is first
+            // Round to the next DPI
+            dpi_pkt[pos++] = newdpi->y[i] / 50;
+            dpi_pkt[pos++] = newdpi->x[i] / 50;
+        }
+
+        // Add sniper
+        dpi_pkt[8] = newdpi->y[0] / 50;
+        dpi_pkt[9] = newdpi->x[0] / 50;
+
+        usbsend_control(kb, dpi_pkt, 10, 174, 0x0000, 0);
     }
 
-    // Add sniper
-    dpi_pkt[8] = newdpi->y[0] / 50;
-    dpi_pkt[9] = newdpi->x[0] / 50;
-    
-    usbsend_control(kb, dpi_pkt, 10, 174, 0x0000, 0);
+    // If the DPI stage differs, set the new one
+    if(newdpi->current != lastdpi->current)
+    {
+        // Sniper is 4 instead of 0. Everything else remains the same.
+        uchar dpistage = (newdpi->current ? newdpi->current : 0x04);
+        uchar dpisetpkt[] = {0x01, dpistage, 0x04, 0x03, 0x00, 0x00, 0x01, 0x00, 0x00, 0x01};
+        usbsend_control(kb, dpisetpkt, 10, 3, 0x0000, 0);
+    }
     
     memcpy(lastdpi, newdpi, sizeof(dpiset));
     return 0;

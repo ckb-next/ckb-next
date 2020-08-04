ac35f7ae665ff7ed283b71ac663dda5e70bf0c86
Tasos Sahanidis <tasos@tasossah.com>
2020-08-05 01:23:47 +0300

    Fix various threading and data race issues
    Mutexes are locked and unlocked in the correct order
    A warning is now being thrown whenever the main thread tries to
    join itself
    Added optional debug logging
    Other tsan warnings have been fixed

--- src/daemon/device.h
+++ src/daemon/device.h
@@ -3,6 +3,7 @@
 
 #include "includes.h"
 #include "keymap.h"
+#include <ckbnextconfig.h>
 
 // Connected devices
 // If this gets raised to a double digit number, the changes introduced by commit f24988ab68a94 will need to be modified to accommodate for that
@@ -14,13 +15,18 @@ extern usbdevice keyboard[DEV_MAX];
 #else
 #define IS_CONNECTED(kb) ((kb) && (kb)->handle && (kb)->event)
 #endif
+#ifdef DEBUG_MUTEX
+#define MUTEX_DBG(str, kb, mutexarray) (ckb_info(str " accessed in %s:%d (%s) at ckb%d, TID 0x%lx, MID %p\n", __FILE__, __LINE__, __func__, INDEX_OF(kb, keyboard), pthread_self(), mutexarray + INDEX_OF(kb, keyboard)) & 0)
+#else
+#define MUTEX_DBG(a, b, c) 0
+#endif
 // A mutex used for USB controls. Needs to be locked before reading or writing the device handle or accessing its profile
 extern pthread_mutex_t devmutex[DEV_MAX];
-#define dmutex(kb) (devmutex + INDEX_OF(kb, keyboard))
+#define dmutex(kb) (devmutex + INDEX_OF(kb, keyboard) + MUTEX_DBG("DMUTEX", kb, devmutex))
 // Similar, but for key input. Also needs to be locked before accessing output FIFOs.
 // When adding or removing a device you must lock BOTH mutexes, dmutex first.
 extern pthread_mutex_t inputmutex[DEV_MAX];
-#define imutex(kb) (inputmutex + INDEX_OF(kb, keyboard))
+#define imutex(kb) (inputmutex + INDEX_OF(kb, keyboard) + MUTEX_DBG("IMUTEX", kb, inputmutex))
 
 // Needed to synchronize sending macro-keys to the os and sending color info to the device
 extern pthread_mutex_t macromutex[DEV_MAX];

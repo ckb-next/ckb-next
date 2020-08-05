4c070c5d3619416d5c7866ce29f546911ffe8faa
Tasos Sahanidis <tasos@tasossah.com>
2020-08-05 06:02:44 +0300

    Replace AppIndicator with minimal KStatusNotifier

--- src/gui/ckbsystemtrayicon.cpp
+++ src/gui/ckbsystemtrayicon.cpp
@@ -1,15 +1,50 @@
+// Cmake will not build this file if USE_DBUS_MENU is not set
 #include "ckbsystemtrayicon.h"
-#include <QEvent>
-#include <QWheelEvent>
+#include <QDir>
+#include <QDebug>
+CkbSystemTrayIcon::CkbSystemTrayIcon(const QIcon& icon, QObject*parent) : KStatusNotifierItem("ckb-next", parent), previousPath("")
+{
+    setIcon(icon);
+}
 
-bool CkbSystemTrayIcon::event(QEvent* evt){
-    if(evt->type() == QEvent::Wheel) {
-        QWheelEvent* wheelEvt = static_cast<QWheelEvent*>(evt);
-        if(wheelEvt->delta() > 0)
-            emit wheelScrolled(true);
-        else
-            emit wheelScrolled(false);
-        return true;
+// Not all implementations support passing icons by pixmap
+// Save the icon to /tmp/ and use that instead
+void CkbSystemTrayIcon::setIcon(QIcon icon)
+{
+    QList<QSize> availSizes = icon.availableSizes();
+    if(!availSizes.length())
+        return;
+
+    QPixmap pm = icon.pixmap(availSizes.at(0));
+    QString path;
+    // Keep trying until we find a path that doesn't conflict
+    int i;
+    for(i = 0; i < 5; i++)
+    {
+        path = QDir::tempPath() + QString("/ckb-next-tray-%1.png").arg(qrand());
+        if(!QFile::exists(path))
+            break;
     }
-    return QSystemTrayIcon::event(evt);
+
+    if(i == 5)
+        return;
+
+    if(!pm.save(path))
+        return;
+
+    setIconByName(path);
+
+    // Delete the old file
+    QFile f(previousPath);
+    if(!previousPath.isEmpty() && f.exists())
+        f.remove();
+
+    previousPath = path;
+}
+
+CkbSystemTrayIcon::~CkbSystemTrayIcon()
+{
+    QFile f(previousPath);
+    if(f.exists())
+        f.remove();
 }
--- src/gui/ckbsystemtrayicon.h
+++ src/gui/ckbsystemtrayicon.h
@@ -1,18 +1,46 @@
 #ifndef CKBSYSTEMTRAYICON_H
 #define CKBSYSTEMTRAYICON_H
+#include <QIcon>
 #include <QSystemTrayIcon>
 
+#ifdef USE_DBUS_MENU
+#include "kstatusnotifier/kstatusnotifieritem.h"
+class CkbSystemTrayIcon : public KStatusNotifierItem {
+#else
+#include <QEvent>
+#include <QWheelEvent>
 class CkbSystemTrayIcon : public QSystemTrayIcon {
+#endif
     Q_OBJECT
 
     public:
-        CkbSystemTrayIcon(const QIcon& icon, QObject* parent = 0)
-                    : QSystemTrayIcon(icon, parent) {}
+#ifdef USE_DBUS_MENU
+        CkbSystemTrayIcon(const QIcon& icon, QObject* parent = 0);
+        inline void show() { setStatus(KStatusNotifierItem::Active); }
+        inline void setVisible(bool visible) { setStatus((visible ? KStatusNotifierItem::Active : KStatusNotifierItem::Passive)); }
+        void setIcon(QIcon icon);
+        ~CkbSystemTrayIcon();
+signals:
+        // This is never emitted by KStatusNotifierItem
+        void activated(QSystemTrayIcon::ActivationReason);
+    private:
+        QString previousPath;
+#else
+        CkbSystemTrayIcon(const QIcon& icon, QObject* parent = 0) :  QSystemTrayIcon(icon, parent) {}
+        virtual bool event(QEvent* evt)
+        {
+            if(evt->type() == QEvent::Wheel) {
+                QWheelEvent* wheelEvt = static_cast<QWheelEvent*>(evt);
+                emit scrollRequested(wheelEvt->delta(), wheelEvt->orientation());
+                return true;
+            }
+            return QSystemTrayIcon::event(evt);
+        }
 
-    virtual bool event(QEvent* evt);
+        signals:
+            void scrollRequested(int delta, Qt::Orientation orientation);
 
-    signals:
-        void wheelScrolled(bool up);
-
+#endif
 };
+
 #endif // CKBSYSTEMTRAYICON_H

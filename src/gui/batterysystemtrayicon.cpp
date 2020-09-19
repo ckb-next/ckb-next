aac2f09d3ed10d5f4cae46fd5cb9af679e461bb4
OrsanZ <33835171+OrsanZ@users.noreply.github.com>
2020-09-19 14:02:18 +0200

    Added scaled battery icons

--- src/gui/batterysystemtrayicon.cpp
+++ src/gui/batterysystemtrayicon.cpp
@@ -4,15 +4,16 @@
 
 void BatteryStatusTrayIcon::setBattery(uint battery, uint charging){
   QString iconName = charging == 2 ? QString("charging") : QString::number(battery);
-  QIcon icon = QIcon(QString(":/img/battery_%1.png")
+  QIcon icon = QIcon(QString(":/img/ckb-next_battery_%1.png")
     .arg(iconName));
-  QString tooltip = QString("%1: %2, %3")
-    .arg(name)
+  QString tooltip = QString("%1:")
+    .arg(name);
+  QString subtooltip = QString("%2, %3")
     .arg(BATTERY_VALUES[battery])
     .arg(BATTERY_CHARGING_VALUES[charging]);
 
-  setIcon(icon, QString("ckb-next-battery-%1").arg(iconName));
-  setToolTip(icon, tooltip, "");
+  setIcon(icon, QString("ckb-next_battery_%1").arg(iconName));
+  setToolTip(icon, tooltip, subtooltip);
 }
 
 const QString BatteryStatusTrayIcon::BATTERY_VALUES[5] = {tr("Not connected"), tr("Critical"), tr("Low"), tr("Medium"), tr("High")};

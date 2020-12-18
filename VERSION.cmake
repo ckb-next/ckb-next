7a86533ac7fab772543b418a1bedc2c41a6b61e2
Tasos Sahanidis <tasos@tasossah.com>
2020-12-18 06:43:08 +0200

    Disable release flag

--- VERSION.cmake
+++ VERSION.cmake
@@ -3,7 +3,7 @@ set(ckb-next_VERSION_MAJOR 0)
 set(ckb-next_VERSION_MINOR 4)
 set(ckb-next_VERSION_PATCH 3)
 # This should be set to TRUE _only_ in archive/tarball releases
-set(ckb-next_VERSION_IS_RELEASE TRUE)
+set(ckb-next_VERSION_IS_RELEASE FALSE)
 
 # Profile format version
 set(ckb-next_PROFILE_VER 3)

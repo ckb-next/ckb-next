50ee67dde605f49efa50c3cb7a52ad62de689656
Tasos Sahanidis <tasos@tasossah.com>
2021-03-18 16:17:01 +0200

    Revert release flag

--- VERSION.cmake
+++ VERSION.cmake
@@ -3,7 +3,7 @@ set(ckb-next_VERSION_MAJOR 0)
 set(ckb-next_VERSION_MINOR 4)
 set(ckb-next_VERSION_PATCH 4)
 # This should be set to TRUE _only_ in archive/tarball releases
-set(ckb-next_VERSION_IS_RELEASE TRUE)
+set(ckb-next_VERSION_IS_RELEASE FALSE)
 
 # Profile format version
 set(ckb-next_PROFILE_VER 3)

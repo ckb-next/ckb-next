2fbc65d5e066d8e2d230e7d533ca929f750bb556
Michael Niedermayer <michael@niedermayer.cc>
2021-08-29 16:27:51 +0200

    daemon/led_bragi: Set or Clear brightness depending on LED changes
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- src/daemon/led_bragi.c
+++ src/daemon/led_bragi.c
@@ -52,6 +52,18 @@ static int updatergb_bragi(usbdevice* kb, int force, const size_t led_offset){
 
     uchar pkt[BRAGI_JUMBO_SIZE] = {0};
 
+    // Since the blank pkt is used to check if the lights are off, we need to make sure it's sufficiently large
+    static_assert(LED_MOUSE <= sizeof(pkt), "pkt is not large enough to check if all zones are off");
+    // Switch LEDs off if its all black, because being able to just switch them off even in hw mode is really nice
+    int newon  = memcmp( newlight->r + led_offset, pkt, CPY_SZ(r)) ||
+                 memcmp( newlight->g + led_offset, pkt, CPY_SZ(g)) ||
+                 memcmp( newlight->b + led_offset, pkt, CPY_SZ(b));
+    int laston = memcmp(lastlight->r + led_offset, pkt, CPY_SZ(r)) ||
+                 memcmp(lastlight->g + led_offset, pkt, CPY_SZ(g)) ||
+                 memcmp(lastlight->b + led_offset, pkt, CPY_SZ(b));
+    if (newon != laston || force)
+        bragi_set_property(kb, BRAGI_BRIGHTNESS, newon ? 1000 : 0);
+
     static_assert(sizeof(pkt) >= 7 + N_KEYS_EXTENDED * 3, "Bragi RGB packet must be large enough to fit all possible zones in the keymap");
 
     memcpy(pkt + 7, newlight->r + led_offset, CPY_SZ(r));

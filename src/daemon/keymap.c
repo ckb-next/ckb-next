80122bda672b17dfa62cc30d7cc9a806eb5f4ed9
Tasos Sahanidis <tasos@tasossah.com>
2021-10-25 14:26:30 +0300

    Split bragi keyboard input
    This is needed to get SW key input in the K57 WL and possibly others
    as unfortunately the header "01 02" in bragi means device 1
    key input, but it just means "NKRO input" in NXP and legacy.
    
    Perhaps the size check could be moved to the top, but there's no
    reason to risk breaking older devices.

--- src/daemon/keymap.c
+++ src/daemon/keymap.c
@@ -671,23 +671,26 @@ void process_input_urb(void* context, unsigned char* buffer, int urblen, ushort 
                 }
             } else {
                 // Assume Keyboard for everything else for now
-                // Accept NKRO only if device is not active
-                if(firstbyte == NKRO_KEY_IN || firstbyte == NKRO_MEDIA_IN) {
-                    if(!targetkb->active){
-                        if(kb->protocol == PROTO_BRAGI)
-                            handle_bragi_key_input(targetkb->input.keys, buffer, urblen);
-                        else
-                            hid_kb_translate(targetkb->input.keys, urblen, buffer, 0);
-                    }
-                } else if(urblen == MSG_SIZE) {
-                    // Skip the bragi header
-                    if(kb->protocol == PROTO_BRAGI)
-                        buffer += 2;
-                    else if((kb->fwversion >= 0x130 || IS_V2_OVERRIDE(kb)) && firstbyte == CORSAIR_IN) // Ugly hack due to old FW 1.15 packets having no header
-                        buffer++;
-                    corsair_kbcopy(targetkb->input.keys, buffer);
-                } else
-                    ckb_err("Unknown data received in input thread %02x from endpoint %02x", firstbyte, ep);
+
+                // We need to split the bragi code because the size check needs to go first
+                if(kb->protocol == PROTO_BRAGI){
+                    if(targetkb->active && urblen == kb->out_ep_packet_size)
+                        corsair_kbcopy(targetkb->input.keys, buffer + 2);
+                    else if(firstbyte == NKRO_KEY_IN || firstbyte == NKRO_MEDIA_IN)
+                        handle_bragi_key_input(targetkb->input.keys, buffer, urblen);
+                    else
+                        ckb_err("Unknown bragi input received %02x from endpoint %02x", firstbyte, ep);
+                } else {
+                    // Accept NKRO only if device is not active
+                    if(firstbyte == NKRO_KEY_IN || firstbyte == NKRO_MEDIA_IN) {
+                        hid_kb_translate(targetkb->input.keys, urblen, buffer, 0);
+                    } else if(urblen == MSG_SIZE) {
+                        if((kb->fwversion >= 0x130 || IS_V2_OVERRIDE(kb)) && firstbyte == CORSAIR_IN) // Ugly hack due to old FW 1.15 packets having no header
+                            buffer++;
+                        corsair_kbcopy(targetkb->input.keys, buffer);
+                    } else
+                        ckb_err("Unknown data received in input thread %02x from endpoint %02x", firstbyte, ep);
+                }
             }
         }
         ///

75f107f228354e1992838083ccf7fa37b9a85204
Tasos Sahanidis <tasos@tasossah.com>
2023-03-29 05:41:36 +0300

    Fix K55 RGB PRO lighting zones
    It seems to only have 5 zones, but internally it claims to have 6.

--- src/daemon/led_bragi.c
+++ src/daemon/led_bragi.c
@@ -36,7 +36,7 @@ static inline size_t bragi_led_count(usbdevice* kb){
     LED_CASE_M(P_KATAR_PRO_XT, 1);
     LED_CASE_M(P_KATAR_PRO, 1);
     LED_CASE_M(P_M55_RGB_PRO, 2);
-    LED_CASE_K(P_K55_PRO, 5);
+    LED_CASE_K(P_K55_PRO, 6);
     LED_CASE_K(P_K55_PRO_XT, 137);
     default:
         ckb_err("Unknown product 0x%hx", kb->product);

0649d7c47001d771a27ba13330ff52b81d4104c0
Tasos Sahanidis <tasos@tasossah.com>
2023-08-03 21:45:22 +0300

    daemon: Fix Dark Core RGB PRO keymap patch

--- src/daemon/keymap_patch.c
+++ src/daemon/keymap_patch.c
@@ -64,21 +64,19 @@ static const keypatch k55proxtpatch[] = {
 };
 
 static const keypatch DCRGBPpatch[] = {
-    { 209, "mouse5",  LED_MOUSE + 1, SCAN_MOUSE | BTN_EXTRA }, // "bar0"
-    { 208, "mouse4",  LED_MOUSE + 2, SCAN_MOUSE | BTN_SIDE  }, // "bar1"
-    { 228, 0,  -1, KEY_NONE },
-    { 213, "profdn",  -1, KEY_CORSAIR },
-    { 238, "back",    LED_MOUSE + 6, KEY_NONE }, // "logo"
-    { 239, 0,     -1, KEY_NONE },
-    { 240, "wheel",   LED_MOUSE + 0, KEY_NONE },
-    { 241, "thumb",   LED_MOUSE + 5, KEY_NONE },
-    { 242, "side",    LED_MOUSE + 7, KEY_NONE },
-    { 243, "dpiw0",    LED_MOUSE + 11, KEY_NONE }, // sniper led
-    { 244, "dpiw1",    LED_MOUSE + 8, KEY_NONE },
-    { 245, "dpiw2",    LED_MOUSE + 9, KEY_NONE },
-    { 246, "dpiw3",    LED_MOUSE +10, KEY_NONE },
-    { 247, "bar3",    LED_MOUSE + 3, KEY_NONE }, // "bar2"
-    { 248, "bar4",    LED_MOUSE + 4, KEY_NONE }, // "bar3"
+    { 228, "mouse4",  LED_MOUSE + 2, SCAN_MOUSE | BTN_SIDE  }, // "bar1"
+    { 229, "mouse5",  LED_MOUSE + 1, SCAN_MOUSE | BTN_EXTRA }, // "bar0"
+    { 233, "profdn",             -1, KEY_CORSAIR },
+    { 258, "back",    LED_MOUSE + 6, KEY_NONE }, // "logo"
+    { 260, "wheel",   LED_MOUSE + 0, KEY_NONE },
+    { 261, "thumb",   LED_MOUSE + 5, KEY_NONE },
+    { 262, "side",    LED_MOUSE + 7, KEY_NONE },
+    { 263, "dpiw0",   LED_MOUSE + 11, KEY_NONE }, // sniper led
+    { 264, "dpiw1",   LED_MOUSE + 8, KEY_NONE },
+    { 265, "dpiw2",   LED_MOUSE + 9, KEY_NONE },
+    { 266, "dpiw3",   LED_MOUSE +10, KEY_NONE },
+    { 267, "bar3",    LED_MOUSE + 3, KEY_NONE }, // "bar2"
+    { 268, "bar4",    LED_MOUSE + 4, KEY_NONE }, // "bar3"
 };
 
 keypatch k100patch[] = {

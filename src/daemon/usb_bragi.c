32699cb27f38c12f849f4e1beb4c623a6c9ea73a
Patricio Rossi <meduzapat@netscape.net>
2025-03-17 15:25:31 -0400

    Adds Support for the k70 PRO Optical Mechanical variant

--- src/daemon/usb_bragi.c
+++ src/daemon/usb_bragi.c
@@ -37,6 +37,7 @@ void bragi_fill_input_eps(usbdevice* kb)
             case P_K65_MINI:
             case P_K70_TKL:
             case P_K70_PRO:
+            case P_K70_PRO_OPTIC:
                 kb->bragi_out_ep = 0x1;
                 kb->bragi_in_ep = 0x82;
                 break;

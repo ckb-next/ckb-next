50ce5bb6eb4fba28774d476f44bad1c4360cfed5
Tasos Sahanidis <tasos@tasossah.com>
2025-03-17 19:54:36 +0200

    v0.6.2

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,9 +1,9 @@
-set(ckb-next_VERSION "0.6.1")
+set(ckb-next_VERSION "0.6.2")
 set(ckb-next_VERSION_MAJOR 0)
 set(ckb-next_VERSION_MINOR 6)
-set(ckb-next_VERSION_PATCH 1)
+set(ckb-next_VERSION_PATCH 2)
 # This should be set to TRUE _only_ in archive/tarball releases
-set(ckb-next_VERSION_IS_RELEASE FALSE)
+set(ckb-next_VERSION_IS_RELEASE TRUE)
 
 # Profile format version
 set(ckb-next_PROFILE_VER 3)

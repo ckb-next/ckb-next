1505a33e8c22e2e89ac11c2461993dd33a405c90
Joe MacDonald <joe@deserted.net>
2025-11-30 19:47:19 -0500

    daemon: add k70_max to recognized list for led_bragi
    Signed-off-by: Joe MacDonald <joe@deserted.net>

--- src/daemon/led_bragi.c
+++ src/daemon/led_bragi.c
@@ -53,6 +53,7 @@ static inline size_t bragi_led_count(usbdevice* kb){
     LED_CASE_K(P_K70_CORE_RGB, 123);
     LED_CASE_K(P_K70_CORE_RGB_2, 123);
     LED_CASE_K(P_K70_CORE_RGB_3, 123);
+    LED_CASE_K(P_K70_MAX, 116);
     LED_CASE_M(P_SCIMITAR_ELITE_BRAGI, 5);
 
     default:
